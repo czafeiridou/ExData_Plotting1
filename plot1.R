8404ed2ab46d9148c6f43e16f524dbf49a9a91c0
czafeiridou <55961599+czafeiridou@users.noreply.github.com>
2019-11-15 18:17:44 -0500

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,26 @@
+# remove everything from environment
+rm(list=ls(all=TRUE))
+
+# Read and set the data
+#read data
+data <- read.delim("C:/Users/czafe/Desktop/R lessons/plotting/exdata_data_household_power_consumption/household_power_consumption.txt", 
+                       header = TRUE, sep = ";", na.strings = "?", 
+                       colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+
+# convert the Date and Time variables to Date/Time classes in R using the strptime() and as.Date() functions
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+# filter out these dates 2007-02-01 and 2007-02-02 to make the data size smaller and keep relevant data
+data <- subset(data, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+
+# for time we need to combine date and time, else it will use today's date
+DateTime <- paste(data$Date, data$Time)
+data <- cbind(DateTime, data)
+data$DateTime <- as.POSIXct(data$DateTime)
+
+# Plot 1
+hist(data$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+
+# Save as png 
+dev.copy(png, "C:/Users/czafe/Desktop/R lessons/plotting/plot1.png", width = 480, height = 480, units = "px")
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,27 @@
+# remove everything from environment
+rm(list=ls(all=TRUE))
+
+# Read and set the data
+#read data
+data <- read.delim("C:/Users/czafe/Desktop/R lessons/plotting/exdata_data_household_power_consumption/household_power_consumption.txt", 
+                   header = TRUE, sep = ";", na.strings = "?", 
+                   colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+
+# convert the Date and Time variables to Date/Time classes in R using the strptime() and as.Date() functions
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+# filter out these dates 2007-02-01 and 2007-02-02 to make the data size smaller and keep relevant data
+data <- subset(data, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+
+# for time we need to combine date and time, else it will use today's date
+DateTime <- paste(data$Date, data$Time)
+data <- cbind(DateTime, data)
+data$DateTime <- as.POSIXct(data$DateTime)
+
+# Plot 2
+plot(data$Global_active_power ~ data$DateTime, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
+
+
+# Save as png 
+dev.copy(png, "C:/Users/czafe/Desktop/R lessons/plotting/plot2.png", width = 480, height = 480, units = "px")
+dev.off()
